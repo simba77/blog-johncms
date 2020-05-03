7c232a7cb99e98ef7ff1e0c9942a0e59fad7de09
Maksim Masalov <simba7@inbox.ru>
2020-05-03 23:25:17 +0300

    Для текста предпросмотра убрана очистка от html тегов

--- modules/blog/lib/Actions/Article.php
+++ modules/blog/lib/Actions/Article.php
@@ -106,7 +106,7 @@ class Article extends AbstractAction
                 'code'         => $this->request->getPost('code', '', FILTER_SANITIZE_STRING),
                 'keywords'     => $this->request->getPost('keywords', '', FILTER_SANITIZE_STRING),
                 'description'  => $this->request->getPost('description', '', FILTER_SANITIZE_STRING),
-                'preview_text' => $this->request->getPost('preview_text', '', FILTER_SANITIZE_STRING),
+                'preview_text' => $this->request->getPost('preview_text', ''),
                 'text'         => $this->request->getPost('text', ''),
             ],
         ];
@@ -190,7 +190,7 @@ class Article extends AbstractAction
                 'code'         => $this->request->getPost('code', $article->code, FILTER_SANITIZE_STRING),
                 'keywords'     => $this->request->getPost('keywords', $article->keywords, FILTER_SANITIZE_STRING),
                 'description'  => $this->request->getPost('description', $article->description, FILTER_SANITIZE_STRING),
-                'preview_text' => $this->request->getPost('preview_text', $article->preview_text, FILTER_SANITIZE_STRING),
+                'preview_text' => $this->request->getPost('preview_text', $article->preview_text),
                 'text'         => $this->request->getPost('text', $article->text),
             ],
         ];
